090c68ed8c185282c4bd2ce402cca74fef6f0d76
Sonson296 <Hussien.a.m.talha@gmail.com>
2023-10-24 12:41:36 +0300

    saving...

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/to-do.html');
 });
 
 // Connect to MongoDB
